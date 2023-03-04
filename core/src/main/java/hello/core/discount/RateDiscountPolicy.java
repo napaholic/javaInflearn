36f22fce5fc25983b3bdf11382d68cc44da9b987
mac-jay <jaewkim@student.42seoul.kr>
2023-03-05 03:49:42 +0900

    primary and qualifier add

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -3,10 +3,12 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("mainDiscountPolicy")
+//@Qualifier("mainDiscountPolicy")//이름으로 주입 단점은 이름이 바뀌면 다 바꿔줘야함
+@Primary//Primary가 우선권을 가짐
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10; // 10% 할인
