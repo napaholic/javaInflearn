7725002a431408167f44a78b15d7fe9323a53616
mac-jay <jaewkim@student.42seoul.kr>
2023-04-04 01:54:32 +0900

    members.jsp add

--- springMVC1/demo/src/main/java/hello/servlet/web/servlet/MemberListServlet.java
+++ springMVC1/demo/src/main/java/hello/servlet/web/servlet/MemberListServlet.java
@@ -29,18 +29,18 @@ public class MemberListServlet extends HttpServlet {
 
 		w.write("<html>");
 		w.write("<head>");
-		w.write(" <meta charset=\"UTF-8\">");
-		w.write(" <title>Title</title>");
+		w.write("	<meta charset=\"UTF-8\">");
+		w.write("	<title>Title</title>");
 		w.write("</head>");
 		w.write("<body>");
 		w.write("<a href=\"/index.html\">메인</a>");
 		w.write("<table>");
-		w.write(" <thead>");
-		w.write(" <th>id</th>");
-		w.write(" <th>username</th>");
-		w.write(" <th>age</th>");
-		w.write(" </thead>");
-		w.write(" <tbody>");
+		w.write("	<thead>");
+		w.write("	<th>id</th>");
+		w.write("	<th>username</th>");
+		w.write("	<th>age</th>");
+		w.write("	</thead>");
+		w.write("	<tbody>");
 /*
  w.write(" <tr>");
  w.write(" <td>1</td>");
@@ -49,13 +49,13 @@ public class MemberListServlet extends HttpServlet {
  w.write(" </tr>");
 */
 		for (Member member : members) {
-			w.write(" <tr>");
-			w.write(" <td>" + member.getId() + "</td>");
-			w.write(" <td>" + member.getUsername() + "</td>");
-			w.write(" <td>" + member.getAge() + "</td>");
-			w.write(" </tr>");
+			w.write("	<tr>");
+			w.write("		<td>" + member.getId() + "</td>");
+			w.write("		<td>" + member.getUsername() + "</td>");
+			w.write("		<td>" + member.getAge() + "</td>");
+			w.write("	</tr>");
 		}
-		w.write(" </tbody>");
+		w.write("	</tbody>");
 		w.write("</table>");
 		w.write("</body>");
 		w.write("</html>");
---
+++ springMVC1/demo/src/main/webapp/jsp/members/new-form.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mjay
+  Date: 2023/04/01
+  Time: 21:29
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <form action="/jsp/members/save.jsp" method="post">
+    username: <input type="text" name="username"/>
+    age: <input type="text" name="age"/>
+    <button type="submit">전송</button>
+    </form>
+</body>
+</html>
