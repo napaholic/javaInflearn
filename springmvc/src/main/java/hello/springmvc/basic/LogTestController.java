879e2d24a5d9dfdd115fee80bd422069d46aed9b
mac-jay <jaewkim@student.42seoul.kr>
2023-04-12 10:05:15 +0900

    add functions about spring

---
+++ springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,26 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j // => private final Logger log = LoggerFactory.getLogger(getClass()); 대체 가능
+@RestController
+public class LogTestController {
+	//private final Logger log = LoggerFactory.getLogger(getClass()); => @Slf4j annotation 으로 대체 가능
+
+	@RequestMapping("/log-test")
+	public String LogTest() {
+		String name = "Spring";
+
+		log.trace("trace log={}", name);
+		log.debug("debug log={}", name);
+		log.info("info log={}", name);
+		log.warn("warn log={}", name);
+		log.error("error log={}", name);
+
+		return "ok";
+	}
+}
