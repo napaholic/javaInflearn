47e5f71696b1573d0bf36c6699347537584f0cba
mac-jay <jaewkim@student.42seoul.kr>
2023-05-02 15:22:56 +0900

    add service logic

---
+++ jpaUtil1/jpaShop/src/main/java/jpabook/jpaShop/service/ItemService.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor // final이 있는 필드만 가지고 생성자를 만들어줌
+public class ItemService {
+
+	private final ItemRepository itemRepository;
+
+	@Transactional // readOnly = false 로 바꿔줌 가장 가까운 것이 우선권을 가짐
+	public void saveItem(Item item) {
+		itemRepository.save(item);
+	}
+
+	public List<Item> findItems() {
+		return itemRepository.findAll();
+	}
+
+	public Item findOne(Long itemId) {
+		return itemRepository.findOne(itemId);
+	}
+}
--- jpaUtil1/jpaShop/src/test/java/jpabook/jpaShop/service/MemberServiceTest.java
+++ jpaUtil1/jpaShop/src/test/java/jpabook/jpaShop/service/MemberServiceTest.java
@@ -6,20 +6,22 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Transactional
+@RunWith(SpringRunner.class) //스프링과 테스트 통합
+@SpringBootTest //스프링 컨테이너 안에서 테스트 실행 Autowired 사용 위해
+@Transactional //테스트 케이스에 있으면 테스트 시작 전에 트랜잭션 시작하고 테스트 완료 후에 항상 롤백
 public class MemberServiceTest {
 
 	@Autowired MemberService memberService;
 	@Autowired MemberRepository memberRepository;
 
 	@Test
+	@Rollback(false)
 	public void 회원가입() throws Exception {
 		//given
 		Member member = new Member();
@@ -32,12 +34,26 @@ public class MemberServiceTest {
 		assertEquals(member, memberRepository.findOne(saveId));
 	}
 
-	@Test
+	@Test(expected = IllegalStateException.class)
 	public void 중복_회원_예외() throws Exception {
 		//given
+		Member member1 = new Member();
+		member1.setName("kim");
+
+		Member member2 = new Member();
+		member2.setName("kim");
 
 		//when
+		memberService.join(member1);
+//		try {
+//			memberService.join(member2); //예외가 발생해야 한다.
+//		} catch (IllegalStateException e) {
+//			return;
+//		}
+		//expected = IllegalStateException.class 가 하위 join 이 던지는 예외를 잡아줌
+		memberService.join(member2);
 
 		//then
+		fail("예외가 발생해야 한다.");
 	}
 }
