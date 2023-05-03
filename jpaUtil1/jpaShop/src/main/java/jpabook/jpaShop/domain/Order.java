83838d5d457399f2048ea9b0fc33df12c85d3332
mac-jay <jaewkim@student.42seoul.kr>
2023-05-03 21:38:34 +0900

    add business logic for Order

--- jpaUtil1/jpaShop/src/main/java/jpabook/jpaShop/domain/Order.java
+++ jpaUtil1/jpaShop/src/main/java/jpabook/jpaShop/domain/Order.java
@@ -64,4 +64,33 @@ public class Order {
 		order.setOrderDate(LocalDateTime.now());
 		return order;
 	}
+
+	//==비즈니스 로직==//
+	/**
+	 * 주문 취소
+	 */
+	public void cancel() {
+		if (delivery.getStatus() == DeliveryStatus.COMP) {
+			throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+		}
+
+		this.setStatus(OrderStatus.CANCEL);
+		for (OrderItem orderItem : this.orderItems) {
+			orderItem.cancel();
+		}
+	}
+
+	//===조회 로직===//
+	/**
+	 * 전체 주문 가격 조회
+	 */
+	public int getTotalPrice() {
+		int totalPrice = 0;
+		for (OrderItem orderItem : orderItems) {
+			totalPrice += orderItem.getTotalPrice();
+		}
+		return totalPrice;
+		//자바 8 방식
+//		return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+	}
 }
